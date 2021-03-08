35beeb2ad382fae5affddf7577ca6f4bd71e4804
PatrickDCullen <cullenpatrickd@gmail.com>
2021-03-08 16:40:40 -0500

    End Ep 56: Design the Timeline

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,41 +11,25 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    
+
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
-<body class="bg-gray-100 h-screen antialiased leading-none font-sans">
+<body>
     <div id="app">
-        <header class="bg-blue-900 py-6">
-            <div class="container mx-auto flex justify-between items-center px-6">
-                <div>
-                    <a href="{{ url('/') }}" class="text-lg font-semibold text-gray-100 no-underline">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-                <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
-                    @guest
-                        <a class="no-underline hover:underline" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        @if (Route::has('register'))
-                            <a class="no-underline hover:underline" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        @endif
-                    @else
-                        <span>{{ Auth::user()->name }}</span>
+        <section class="px-8 py-4 mb-6">
+            <header class="container mx-auto">
+                <h1>
+                    <img src="https://raw.githubusercontent.com/laracasts/Tweety/master/public/images/logo.svg" alt="Tweety">
+                </h1>
+            </header>
+        </section>
 
-                        <a href="{{ route('logout') }}"
-                           class="no-underline hover:underline"
-                           onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
-                            {{ csrf_field() }}
-                        </form>
-                    @endguest
-                </nav>
-            </div>
-        </header>
-
-        @yield('content')
+        <section class="px-8">
+            <main class="container mx-auto">
+                @yield('content')
+            </main>
+        </section>
     </div>
 </body>
 </html>
